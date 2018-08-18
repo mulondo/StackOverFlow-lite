fabd4930c67314a127efd81d0d6a1fb88b041b70
mulondo <mulondo26@gmail.com>
2018-08-18 09:30:36 +0300

    modified

---
+++ API/app/modal.py
@@ -0,0 +1,16 @@
+class Data_store:
+    available_questions=[{'QuestionID':'Qn1',
+            'QuestionName':'how to print hello world in python',
+            'answers':[ {'answerID':'ans1',
+                            'cantent':'thats very easy man'
+                        },
+                        {   'answerID':'ans2',
+                            'content':'just use print and then helo world double quotes'
+                        }
+                    ]
+            },
+                        
+            {'QuestionID':'Qn2',
+            'QuestionName':'how to make unit tests in python',
+            'answers':[{'answerID':'ans1',
+                        'cantent':'man first read before ask'},{'answerID':'ans2','content':'find the anwser to this link'}]}]
