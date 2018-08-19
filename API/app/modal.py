7a94f4434b777ea8da72136879dbe2cff012af7c
mulondo <mulondo26@gmail.com>
2018-08-19 16:32:39 +0300

    test with pylint

--- API/app/modal.py
+++ API/app/modal.py
@@ -1,16 +1,10 @@
-class Data_store:
-    available_questions=[{'QuestionID':'Qn1',
-            'QuestionName':'how to print hello world in python',
-            'answers':[ {'answerID':'ans1',
-                            'cantent':'thats very easy man'
-                        },
-                        {   'answerID':'ans2',
-                            'content':'just use print and then helo world double quotes'
-                        }
-                    ]
-            },
-                        
-            {'QuestionID':'Qn2',
-            'QuestionName':'how to make unit tests in python',
-            'answers':[{'answerID':'ans1',
-                        'cantent':'man first read before ask'},{'answerID':'ans2','content':'find the anwser to this link'}]}]
+"""
+Data store
+"""
+class Datastore:
+    """
+    Datastore class
+    """
+    available_questions = [
+        {'QuestionID':1, 'QuestionName':'start python', 'Question_owner':'moses', 'answers':[]}
+        ]
