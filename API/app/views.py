87e08d546705a1b4a14628654febdc901a757f5c
mulondo <mulondo26@gmail.com>
2018-08-19 16:41:47 +0300

    add docstrings

--- API/app/views.py
+++ API/app/views.py
@@ -1,20 +1,16 @@
-
+"""api routes"""
 from controllers.api_controller import Api_controller
 
-
 class Routes:
-    """
-    Class to generate urls via static method generate
-    """
-
+    """ class for api routes"""
     @staticmethod
-    def generate(app):
-        """
-        Generate urls on the app context
-        It takes no arguments
-        :param app: takes in the app variable
-        :return: links
-        """
-
-        app.add_url_rule('/api/v1/questions/', view_func=Api_controller.as_view('get_questions'), methods=['GET'],
-                         strict_slashes=False)
+    def generate_routes(app):
+        """generates the routes"""
+        app.add_url_rule('/api/v1/questions', view_func=Api_controller.as_view('get_questions'), methods=['GET'],
+        strict_slashes=False)
+        app.add_url_rule('/api/v1/questions', view_func=Api_controller.as_view('post_question'),
+        methods=['POST'],strict_slashes=False)
+        app.add_url_rule('/api/v1/questions/<int:question_id>/', view_func=Api_controller.as_view('get_a_question'),
+        methods=['GET'],strict_slashes=False)
+        app.add_url_rule('/api/v1/questions/<int:question_id>/answers', view_func=Api_controller.as_view('post_an_answer'), methods=['POST'],strict_slashes=False)
+        
