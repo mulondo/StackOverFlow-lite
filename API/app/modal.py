1062dd3e33cd3dc512f51d8b4e62b7d42af1d2ad
mulondo <mulondo26@gmail.com>
2018-08-22 00:53:32 +0300

    updated the datastore

--- API/app/modal.py
+++ API/app/modal.py
@@ -6,5 +6,6 @@ class Datastore:
     Datastore class
     """
     available_questions = [
-        {'QuestionID':1, 'QuestionName':'start python', 'Question_owner':'moses', 'answers':[]}
+        {'QuestionID':1, 'Question_type':'python', 'QuestionName':'start python', 'Question_description':'How to start programing in python', 'Question_owner':'Hope', 'Question_post_date':'01/09/2018', 'answers':[{'answerID':2, 'content':'first instal it', 
+        'answer_author':'Moses','correctness':'true','answer_post_date':'01/11/2018'}]}
         ]
