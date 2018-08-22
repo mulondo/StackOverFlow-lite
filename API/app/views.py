92f2f8afe83526e74f07e704192f963b387ffae2
mulondo <mulondo26@gmail.com>
2018-08-22 18:41:19 +0300

    solved are with get a question

--- API/app/views.py
+++ API/app/views.py
@@ -6,9 +6,8 @@ class Routes:
     @staticmethod
     def generate_routes(app):
         """generates the routes"""
-        app.add_url_rule('/api/v1/questions', view_func=Api_controller.as_view('get_questions'), methods=['GET'],
-        strict_slashes=False)
-        app.add_url_rule('/api/v1/questions/<int:question_id>', view_func=Api_controller.as_view('get_a_question'), methods=['GET'],
+        app.add_url_rule('/api/v1/questions', view_func=Api_controller.as_view('get_a_questions') ,methods=['GET'], strict_slashes=False)
+        app.add_url_rule('/api/v1/questions/<int:question_id>', view_func=Api_controller.as_view('get_a_quest'), methods=['GET'],
         strict_slashes=False)
         app.add_url_rule('/api/v1/questions', view_func=Api_controller.as_view('post_question'),
         methods=['POST'],strict_slashes=False)       
