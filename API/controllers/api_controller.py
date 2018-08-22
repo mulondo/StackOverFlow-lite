6751f1afe66ac6160d8392b0adc8f4649732529d
mulondo <mulondo26@gmail.com>
2018-08-22 19:28:17 +0300

    made pylint test

--- API/controllers/api_controller.py
+++ API/controllers/api_controller.py
@@ -1,32 +1,32 @@
 """contain the API logi"""
-from flask import request, jsonify, Response, json
+from flask import request, jsonify, Response
 from flask.views import MethodView
 from app.modal import Datastore
 
-questions = Datastore.available_questions
+QUESTIONS = Datastore.available_questions
 class Api_controller(MethodView):
     """ contains methods that perform the logic"""
     @staticmethod
     def get(question_id=None):
-        if str(request.url_rule)=="/api/v1/questions":
+        """ Calls methods that require a get request """
+        if str(request.url_rule) == "/api/v1/questions":
             return Api_controller.get_all_questions()
-        if str(request.url_rule)=="/api/v1/questions/<int:question_id>":
+        if str(request.url_rule) == "/api/v1/questions/<int:question_id>":
             return Api_controller.get_a_question(question_id)
-    
+
     @staticmethod
     def get_all_questions():
-        return jsonify({'questions':questions})
-    
+        """ gets all questions from the data store"""
+        return jsonify({'questions':QUESTIONS})
+
     @staticmethod
-    def get_a_question(question_id):        
-            for one_question in questions:
-                if one_question['QuestionID'] == question_id:
-                    return jsonify(one_question)
-                else:
-                    return jsonify({'Message':'The question is not found'})
-        
-    
-   
+    def get_a_question(question_id):
+        """ gets a specific question from the data store"""
+        for one_question in QUESTIONS:
+            if one_question['QuestionID'] == question_id:
+                return jsonify(one_question)
+            return jsonify({'Message':'The question is not found'})
+
 
     @staticmethod
     def post(question_id=None):
@@ -38,7 +38,7 @@ class Api_controller(MethodView):
         return 'Errors'
 
     @staticmethod
-    def do_post_question(self):
+    def do_post_question():
         """ this enable one to add a question"""
         request_data = request.get_json()
         if valid_question(request_data):
@@ -50,31 +50,27 @@ class Api_controller(MethodView):
                 'Question_description': request_data['Question_description'],
                 'Question_post_date': request_data['Question_post_date'],
                 'answer':[]
-                }      
-            questions.append(question)
-            return jsonify({'questions':questions})
+                }
+            QUESTIONS.append(question)
+            return jsonify({'questions':QUESTIONS})
             #response = Response("", 201, mimetype="application/json")
             #response.headers['Location'] = "questions/" + str(request_data['QuestionID'])
             #return response
-        else:
-            return jsonify({'Message':'Question not created due to incorrect format'})
+        return jsonify({'Message':'Question not created due to incorrect format'})
            # bad_object = {
               #  "error": "Incorrect Question Format",
               #  "use": "{'QuestionID': '4',"
               #  "'QuestionID': '1','QuestionName': 'hello world' }"
            # }
            # response = Response(json.dumps(bad_object), status=400, mimetype="application/json")
-            
-
-
 
     @staticmethod
     def answer_question(question_id):
         """ Adds an answer for a question"""
         request_data = request.get_json()
-        for question in questions:
-            if question['QuestionID'] == question_id:                
-                post_answer={
+        for question in QUESTIONS:
+            if question['QuestionID'] == question_id:
+                post_answer = {
                     'answerID':request_data['answerID'],
                     'content':request_data['content'],
                     'answer_author':request_data['answer_author'],
@@ -82,7 +78,6 @@ class Api_controller(MethodView):
                     'answer_post_date':request_data['answer_post_date']
                 }
                 question['answers'].append(post_answer)
-                
             return jsonify({'Message :':'The resource not found'})
 
 
