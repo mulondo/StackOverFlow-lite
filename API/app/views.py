feb70c9b2814b9d0046e067e8bf08a268138d1ce
mulondo <mulondo26@gmail.com>
2018-08-23 14:44:38 +0300

    little change on end posts

--- API/app/views.py
+++ API/app/views.py
@@ -6,10 +6,8 @@ class Routes:
     @staticmethod
     def generate_routes(app):
         """generates the routes"""
-        app.add_url_rule('/api/v1/questions', view_func=Api_controller.as_view('get_a_questions') ,methods=['GET'], strict_slashes=False)
-        app.add_url_rule('/api/v1/questions/<int:question_id>', view_func=Api_controller.as_view('get_a_quest'), methods=['GET'],
-        strict_slashes=False)
-        app.add_url_rule('/api/v1/questions', view_func=Api_controller.as_view('post_question'),
-        methods=['POST'],strict_slashes=False)       
-        app.add_url_rule('/api/v1/questions/<int:question_id>/answers', view_func=Api_controller.as_view('post_an_answer'), methods=['POST'],strict_slashes=False)
+        app.add_url_rule('/api/v1/questions', view_func=Api_controller.as_view('get_quest'), methods=['GET'], strict_slashes=False)
+        app.add_url_rule('/api/v1/questions/<int:question_id>', view_func=Api_controller.as_view('get_a_quest'), methods=['GET'], strict_slashes=False)
+        app.add_url_rule('/api/v1/questions', view_func=Api_controller.as_view('post_question'), methods=['POST'],strict_slashes=False)
+        app.add_url_rule('/api/v1/questions/<int:question_id>/answers', view_func=Api_controller.as_view('post_an_answer'), methods=['POST'], strict_slashes=False)
         
