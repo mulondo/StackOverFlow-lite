b024f61b6a7ae03c1e58ce929f8305a07be9f425
mulondo <mulondo26@gmail.com>
2018-08-24 17:55:36 +0300

    updated

---
+++ app/models.py
@@ -0,0 +1,15 @@
+class Questions:
+
+    def __init__(self,question_id,question_name):
+        self.question_id=question_id
+        self.question_name = question_name
+        self.questions_ = []
+
+
+    def post_question(self,question_id,question_name):
+        question={self.question_id,self.question_name}
+        return self.questions_.append(questions)
+    
+    def get_all_questions(self)
+        return self.questions_
+    
